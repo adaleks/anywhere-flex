40c41466d9d77bd8f0ec7d408c8d97838b373fc0
Goran Jovanovic <gjovanovic.st@gmail.com>
2021-12-26 16:15:37 +0100

    Delete alll

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-let gulp = require('gulp');
-
-gulp.task('copy-lib', function () {
-    return gulp
-        .src(['src/styles/lib/**/*', 'README.md', 'package.json'])
-        .pipe(gulp.dest('dist/lib'));
-});
--- scripts/dist-package-json.js
+++
@@ -1,9 +0,0 @@
-let DotJson = require('dot-json');
-let packageJSON = new DotJson('dist/lib/package.json');
-
-packageJSON
-    .delete('devDependencies')
-    .delete('dependencies')
-    .delete('scripts')
-    .delete('main')
-    .save();
